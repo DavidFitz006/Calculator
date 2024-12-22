99128e3911c261a8167a938ffc31debb07c81c31
DavidFitz006 <dfitzpatrick992@gmail.com>
2024-12-22 12:00:51 +0000

    Update FireComStreamlit.py

--- FireComStreamlit.py
+++ FireComStreamlit.py
@@ -8,14 +8,14 @@ if len(ArtGrid) == 9 and ArtGrid[4] == '-':
     # Extract the first 4 digits and the last 4 digits
     ArtX = int(ArtGrid[:4])  # First 4 digits
     ArtY = int(ArtGrid[5:])  # Last 4 digits
-    ArtZ = st.number_input("Artillery Elevation (m):")
+    #ArtZ = st.number_input("Artillery Elevation (m):")
 
 TarGrid = st.text_input("Target grid reference (Enter the grid in the format xxxx-xxxx):")
 if len(TarGrid) == 9 and TarGrid[4] == '-':
     # Extract the first 4 digits and the last 4 digits
     TarX = int(TarGrid[:4])  # First 4 digits
     TarY = int(TarGrid[5:])  # Last 4 digits
-    TarZ = st.number_input("Target Elevation (m):")
+    #TarZ = st.number_input("Target Elevation (m):")
 
     # Calculate the distance
     distance = math.sqrt((TarX - ArtX)**2 + (TarY - ArtY)**2)# + (TarZ - ArtZ)**2)
