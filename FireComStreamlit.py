377cd01c725139d7328cfbf4d28de8f2b030864f
DavidFitz006 <dfitzpatrick992@gmail.com>
2025-03-22 11:54:13 +0000

    Update FireComStreamlit.py

--- FireComStreamlit.py
+++ FireComStreamlit.py
@@ -1,21 +1,22 @@
 import streamlit as st
 import math
+import pandas as pd
 
 st.header("Artillery Calculator")
-
+df = pd.DataFrame()
 ArtGrid = st.text_input("Artillery grid reference (Enter the grid in the format xxxx-xxxx):")
 if len(ArtGrid) == 9 and ArtGrid[4] == '-':
     # Extract the first 4 digits and the last 4 digits
     ArtX = int(ArtGrid[:4])  # First 4 digits
     ArtY = int(ArtGrid[5:])  # Last 4 digits
-    #ArtZ = st.number_input("Artillery Elevation (m):")
+    ArtZ = st.number_input("Artillery Elevation (m):")
 
 TarGrid = st.text_input("Target grid reference (Enter the grid in the format xxxx-xxxx):")
 if len(TarGrid) == 9 and TarGrid[4] == '-':
     # Extract the first 4 digits and the last 4 digits
     TarX = int(TarGrid[:4])  # First 4 digits
     TarY = int(TarGrid[5:])  # Last 4 digits
-    #TarZ = st.number_input("Target Elevation (m):")
+    TarZ = st.number_input("Target Elevation (m):")
 
     # Calculate the distance
     distance = math.sqrt((TarX - ArtX)**2 + (TarY - ArtY)**2)# + (TarZ - ArtZ)**2)
@@ -31,10 +32,14 @@ if len(TarGrid) == 9 and TarGrid[4] == '-':
     angle_degrees = math.degrees(angle_radians)
 
     # Normalize the angle to 0-360 degrees
-    bearing = (angle_degrees + 360) % 360
-    st.write("Bearing: ", bearing, "degrees")
+    mils = ((angle_degrees + 360) % 360) * 0.05625
+    st.write("Bearing: ", mils, "mils")
+    
+    elevation_diff = (TarZ - ArtZ) / 100
+    st.write("Elevation difference divided by 100: ", elevation_diff)
+    
+    elevation = st.number_input("Elevation: ")
+    d_elv = st.number_input("D elv per 100m: ")
+    fin_elv = st.write("Final Elevation: ", elevation + d_elv * elevation_diff)
 
-    return 
-    rec_elv = -0.00001078 * distance**2 - 0.0119 * distance + 33.341
-    st.write("Elevation:", rec_elv)
 
